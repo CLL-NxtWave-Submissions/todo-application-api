f9b2f806fa19fad9c14435a6cf511937208bcb71
Surya T N <suryatej.nim@yahoo.com>
2022-08-21 14:17:58 +0000

    Implemented server-side handlers for client requests to API end-points: /todos (GET, POST), /todos/:todoId (GET, PUT, DELETE)

--- app.js
+++ app.js
@@ -62,4 +62,123 @@ app.get("/todos", async (req, res) => {
   res.send(todoItemData);
 });
 
+/*
+    End-Point 2: GET /todos/:todoId
+    ------------
+    To fetch data of specific
+    todo item
+*/
+app.get("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+
+  const queryToFetchSpecificTodoItemData = `
+    SELECT
+        *
+    FROM
+        todo
+    WHERE
+        id = ${todoId};
+    `;
+
+  const specificTodoItemData = await todoApplicationDBConnectionObj.get(
+    queryToFetchSpecificTodoItemData
+  );
+  res.send(specificTodoItemData);
+});
+
+/*
+    End-Point 3: POST /todos
+    ------------
+    To add new todo item data
+    to the todo table
+*/
+app.post("/todos", async (req, res) => {
+  const { id, todo, priority, status } = req.body;
+
+  const queryToAddNewTodoItemData = `
+    INSERT INTO
+        todo (id, todo, priority, status)
+    VALUES
+        (${id}, '${todo}', '${priority}', '${status}');
+    
+    `;
+
+  const addNewTodoDBResponse = await todoApplicationDBConnectionObj.run(
+    queryToAddNewTodoItemData
+  );
+  res.send("Todo Successfully Added");
+});
+
+/*
+    End-Point 4: PUT /todos/:todoId
+    ------------
+    To update data of specific todo
+    with id: todoId
+*/
+app.put("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { todo, priority, status } = req.body;
+
+  const queryToUpdateSpecificTodoItemData = "";
+  const todoUpdateSuccessMsg = "";
+
+  if (todo !== undefined) {
+    queryToUpdateSpecificTodoItemData = `
+        UPDATE
+            todo
+        SET
+            todo = '${todo}'
+        WHERE
+            id = ${todoId};
+        `;
+
+    todoUpdateSuccessMsg = "Todo Updated";
+  } else if (priority !== undefined) {
+    queryToUpdateSpecificTodoItemData = `
+        UPDATE
+            todo
+        SET
+            priority = '${priority}'
+        WHERE
+            id = ${todoId};
+        `;
+
+    todoUpdateSuccessMsg = "Priority Updated";
+  } else if (status !== undefined) {
+    queryToUpdateSpecificTodoItemData = `
+        UPDATE
+            todo
+        SET
+            status = '${status};
+        WHERE
+            id = ${todoId};
+        `;
+
+    todoUpdateSuccessMsg = "Status Updated";
+  }
+
+  await todoApplicationDBConnectionObj.run(queryToUpdateSpecificTodoItemData);
+  res.send(todoUpdateSuccessMsg);
+});
+
+/*
+    End-Point 5: DELETE /todos/:todoId
+    ------------
+    To delete specific todo item 
+    with id: todoId
+*/
+app.delete("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+
+  const queryToDeleteSpecificTodoItem = `
+    DELETE FROM
+        todo
+    WHERE
+        id = ${todoId};
+    `;
+
+  await todoApplicationDBConnectionObj.run(queryToDeleteSpecificTodoItem);
+  res.send("Todo Deleted");
+});
+
 module.exports = app;
